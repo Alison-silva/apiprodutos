1287bc9c0e7db56b9c014c8bb79513cf43059a5d
Alison-silva <alison.ssilva@hotmail.com>
2024-02-17 14:05:44 -0300

    Crud produtos feito

---
+++ apiprodutos/src/main/java/com/alison/apiprodutos/controllers/ProductController.java
@@ -0,0 +1,58 @@
+package com.alison.apiprodutos.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alison.apiprodutos.model.Product;
+import com.alison.apiprodutos.services.ProductsService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+	@Autowired
+	private ProductsService productsService;
+
+	@PostMapping
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		Product newProduct = productsService.save(product);
+		return new ResponseEntity<Product>(newProduct, HttpStatus.CREATED);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Product>> list() {
+		List<Product> product = productsService.list();
+		return new ResponseEntity<List<Product>>(product, HttpStatus.OK);
+	}
+
+	@PutMapping
+	public ResponseEntity<Product> update(@RequestBody Product product) {
+		Product p = productsService.update(product);
+		return new ResponseEntity<Product>(p, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		productsService.delete(id);
+		return "Deletado com sucesso!";
+	}
+
+	@GetMapping(value = "/findIdProd/{id}")
+	public ResponseEntity<Product> findByIdProd(@PathVariable(value = "id") Long id) {
+		Optional<Product> prod = productsService.findByIdProduct(id);
+		return new ResponseEntity<Product>(prod.get(), HttpStatus.OK);
+	}
+
+}
--- apiprodutos/src/main/java/com/alison/apiprodutos/services/ProductsService.java
+++ apiprodutos/src/main/java/com/alison/apiprodutos/services/ProductsService.java
@@ -1,8 +1,38 @@
 package com.alison.apiprodutos.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.alison.apiprodutos.model.Product;
+import com.alison.apiprodutos.repositories.ProductRepository;
 
 @Service
 public class ProductsService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	public Product save(Product product) {
+		return productRepository.save(product);
+	}
+
+	public List<Product> list() {
+		return productRepository.findAll();
+	}
+
+	public Product update(Product product) {
+		return productRepository.saveAndFlush(product);
+	}
+
+	public void delete(Long id) {
+		productRepository.deleteById(id);
+	}
+
+	public Optional<Product> findByIdProduct(Long id) {
+		return productRepository.findById(id);
+	}
 
 }
