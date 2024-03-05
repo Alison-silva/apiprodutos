54c5acee434a14704d629fa5478207b8aa5968f6
Alison-silva <alison.ssilva@hotmail.com>
2024-03-04 23:09:35 -0300

    Testes de salvar, buscar e deletar

--- apiprodutos/src/test/java/com/alison/apiprodutos/ApiprodutosApplicationTests.java
+++ apiprodutos/src/test/java/com/alison/apiprodutos/ApiprodutosApplicationTests.java
@@ -1,13 +1,50 @@
 package com.alison.apiprodutos;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.alison.apiprodutos.model.Category;
+import com.alison.apiprodutos.model.Product;
+import com.alison.apiprodutos.services.ProductsService;
+
+import junit.framework.TestCase;
+
 @SpringBootTest
-class ApiprodutosApplicationTests {
+class ApiprodutosApplicationTests extends TestCase {
+
+	@Autowired
+	private ProductsService productsService;
 
 	@Test
-	void contextLoads() {
+	public void testSave() {
+		Product p = new Product();
+		p.setName("Teste");
+		p.setAmount(2);
+		p.setPrice(BigDecimal.valueOf(500));
+		Category mockCategory = Mockito.mock(Category.class);
+		p.setCategory(mockCategory);
+
+		assertEquals("Teste", p.getName());
+		assertEquals(2, p.getAmount());
+		assertEquals(BigDecimal.valueOf(500), p.getPrice());
+		assertEquals(mockCategory, p.getCategory());
+	}
+
+	@Test
+	public void testFindAll() {
+		List<Product> p = productsService.list();
+		System.out.println(p);
+	}
+
+	@Test
+	public void testDeletar() {
+		Product mock = Mockito.mock(Product.class);
+		productsService.delete(mock.getId());
 	}
 
 }
